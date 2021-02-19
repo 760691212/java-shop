1422864f65fb2c890a287a4f942a4fd6b575f0c5
kxy <760691212@qq.com>
2021-02-19 17:04:37 +0800

    auth

--- auth-shop/src/main/java/com/shop/auth/service/impl/AuthServiceImpl.java
+++ auth-shop/src/main/java/com/shop/auth/service/impl/AuthServiceImpl.java
@@ -3,4 +3,8 @@ package com.shop.auth.service.impl;
 import com.shop.auth.service.AuthService;
 
 public class AuthServiceImpl implements AuthService {
+    @Override
+    public String imPowerUser(String username, String password) {
+        return null;
+    }
 }
